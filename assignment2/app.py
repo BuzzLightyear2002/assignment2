fb74e65f9f331a17902f5b95c0af467bdd53de7f
Raj Chauhan <rajbhavesh2002@gmail.com>
2024-06-27 23:15:24 -0300

    Dropping the table

--- assignment2/app.py
+++ assignment2/app.py
@@ -5,28 +5,25 @@ import mysql.connector
 app = Flask(__name__)
 
 mydb = mysql.connector.connect(
-    host="assignment2-mysql.cl179oaea0jj.us-east-1.rds.amazonaws.com",
+    host="assignment2-db.cl179oaea0jj.us-east-1.rds.amazonaws.com",
     user="admin",
     password="RAJ1972002",
-    database="assignment2_db"
-    )
+    database="assignment2-db"
+)
 
-def create_table():
+def drop_table():
     try:
         cursor = mydb.cursor()
-        cursor.execute("""
-        drop table products
-        """)
+        cursor.execute("DROP TABLE IF EXISTS products")
         mydb.commit()
         cursor.close()
-        return "Table 'products' created successfully or already exists."
+        return "Table 'products' dropped successfully."
     except Exception as e:
-        return f"An error occurred while creating the table: {e}"
+        return f"An error occurred while dropping the table: {e}"
 
-# Ensure the table is created at the start of the application
-validation_message = create_table()
-print(validation_message)  # Print the validation message
-
+# Execute the drop table command
+drop_message = drop_table()
+print(drop_message)  # Print the drop table message
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 80, debug=True)
